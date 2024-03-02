0279ee3dc7e116db972d5b9f7589b7b1fc4a26b2
geproha <33349610+geproha@users.noreply.github.com>
2024-03-02 01:31:09 +0100

    vytvoření funkce pro rozsvícení jednoho sloupce

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
+function statusLED (číslo: number) {
+    for (let pořadí = 0; pořadí <= 4; pořadí++) {
+        led.plot(číslo, pořadí)
+    }
+}
+radio.onReceivedValue(function (name, value) {
+    if (name == "done") {
+        statusLED(value)
+    }
+})
+radio.setGroup(42)
 basic.forever(function () {
 	
 })
