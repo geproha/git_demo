381422351ee654efa01a5dcf80bb07c397954c49
geproha <33349610+geproha@users.noreply.github.com>
2024-03-02 01:43:26 +0100

    přidání melodie do listu (Interstellar)

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 def statusLED(číslo: number):
     for pořadí in range(5):
         led.plot(číslo, pořadí)
+    for index in range(2):
+        music.play(music.tone_playable(melodie[číslo], music.beat(BeatFraction.HALF)),
+            music.PlaybackMode.UNTIL_DONE)
+        music.play(music.tone_playable(659, music.beat(BeatFraction.WHOLE)),
+            music.PlaybackMode.UNTIL_DONE)
 
 def on_received_value(name, value):
     if name == "done":
         statusLED(value)
 radio.on_received_value(on_received_value)
 
+melodie: List[number] = []
+melodie.append(440)
+melodie.append(494)
+melodie.append(523)
+melodie.append(587)
 radio.set_group(42)
-
-def on_forever():
-    pass
-basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,16 +1,20 @@
-function statusLED(číslo: number) {
-    for (let pořadí = 0; pořadí < 5; pořadí++) {
+function statusLED (číslo: number) {
+    for (let pořadí = 0; pořadí <= 4; pořadí++) {
         led.plot(číslo, pořadí)
     }
+    for (let index = 0; index < 2; index++) {
+        music.play(music.tonePlayable(melodie[číslo], music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+        music.play(music.tonePlayable(659, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    }
 }
-
-radio.onReceivedValue(function on_received_value(name: string, value: number) {
+radio.onReceivedValue(function (name, value) {
     if (name == "done") {
         statusLED(value)
     }
-    
 })
+let melodie: number[] = []
+melodie.push(440)
+melodie.push(494)
+melodie.push(523)
+melodie.push(587)
 radio.setGroup(42)
-basic.forever(function on_forever() {
-    
-})
